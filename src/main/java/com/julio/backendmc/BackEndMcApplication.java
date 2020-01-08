69c4f1a1048c9fa3ec03e0aac0d480058f0f1ebb
juliocyg <julito.entey@gmail.com>
2020-01-07 22:39:51 -0500

    Creando operacion de instanciacion

--- src/main/java/com/julio/backendmc/BackEndMcApplication.java
+++ src/main/java/com/julio/backendmc/BackEndMcApplication.java
@@ -1,13 +1,32 @@
 package com.julio.backendmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackEndMcApplication {
+import com.julio.backendmc.domain.Categoria;
+import com.julio.backendmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class BackEndMcApplication implements CommandLineRunner { //CommandLineRunner Permite implementar un metodo auxiliar para ejecutar alguna opcion
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; // Dependencia 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndMcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informatica");//Se instancia datos  para ser llenado a la categoria
+		Categoria cat2 = new Categoria(null, "Escritorio"); 
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
 }
--- src/main/java/com/julio/backendmc/domain/Categoria.java
+++ src/main/java/com/julio/backendmc/domain/Categoria.java
@@ -12,8 +12,8 @@ public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-        @Id
-        @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
