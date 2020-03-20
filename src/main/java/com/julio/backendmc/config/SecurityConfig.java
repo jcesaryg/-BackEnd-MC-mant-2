bb5589ddfe5a2fa81d91609196487a29b7998608
juliocyg <julito.entey@gmail.com>
2020-03-20 09:11:54 -0500

    implementacion de la Autorizacion de los token

--- src/main/java/com/julio/backendmc/config/SecurityConfig.java
+++ src/main/java/com/julio/backendmc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.julio.backendmc.security.JWTAuthenticationFilter;
+import com.julio.backendmc.security.JWTAuthorizationFilter;
 import com.julio.backendmc.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest()
 				.authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
